1e0809b317bdc9d0dbd1c711e066578b4b001a60
davidedallecarbonare94 <davide.dallecarbonare94@gmail.com>
2021-04-26 18:18:43 +0200

    no solution found with for

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,19 +1,19 @@
+/* usando il for */
+
 //Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 var userEmail = prompt("Inserisci qui la tua email")
 console.log(userEmail);
 var registeredEmails = ["davide.dc@gmail.com", "alessandranati.an@gmail.com", "fabiopacifici@gmail.com", "narutouzumaki@gmail.com", "kakashihatake@gmail.com", "gaimaito@gmail.com"]
 console.log(registeredEmails);
 
-for (var i = 0; i < userEmail.length; i++) {
-    if (userEmail != registeredEmails) {
-        alert("Non possiedi le credenziali per l'accesso al sito");
-    } else (userEmail == registeredEmails)
-        alert("Complimenti! Ora puoi accedere al sito!");
-    }
+for (var i = 0; i < userEmail.length; i++){
+    console.log(registeredEmails[i])
+    if (userEmail == registeredEmails[i]) {
+        alert("Complimenti! Ora puoi accedere al sito!")
+    } else alert("Non possiedi le credenziali necessarie.")
+}
 
-
-
-
+/* usando lo switch */
 
 /* switch (userEmail) {
     case registeredEmails[0]:
